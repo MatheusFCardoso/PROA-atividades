ae02854dd69c1493f91f37584d697ae67c822e01
Matheus de França Cardoso <88097181+MatheusFCardoso@users.noreply.github.com>
2022-03-18 17:10:03 -0300

    Término da atividade

--- hora_de_codar2/teste1.js
+++ hora_de_codar2/teste1.js
@@ -1,2 +1,3 @@
 var array = [1,2,3,4,5]
-console.log(array[4])
+console.log(array[3])
+console.log('Olá')
---
+++ hora_de_codar2/teste3.js
@@ -0,0 +1,5 @@
+var array = [1,2,3,4,5,6,7,8,9,10]
+var max = Math.max(...array)
+var min = Math.min(...array)
+
+console.log(max)
